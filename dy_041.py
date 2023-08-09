0a429afbac45f3b1cee557f9b7718fde35b221cd
Muhydeen Adelani <139554441+DataNaija@users.noreply.github.com>
2023-08-09 03:06:22 -0600

    Update dy_041.py

--- dy_041.py
+++ dy_041.py
@@ -1,15 +1,3 @@
-# Grading Student Based on Marks Obtained by Making Functions
-# Suppose you just attended a University examination. 
-# The marks you obtained in various subjects are stored in a list like this:
-
-# You want to find the average marks you obtained in the exam.
-
-# Based on the average marks you want to find your grade as:
-# You will get Grade A if the average marks is equal to or above 80 (A >= 80)
-# You will get Grade B if the average marks is equal to or above 60 and less than 80 (B >= 60 and B < 80)
-# You will get Grade C if the average marks is equal to or above 50 and less than 60 (C >= 50 and < 60)
-# And if the average marks is less than 50, you will get Grade F (F < 50)
-
 def calculate_average(marks):
     total_marks = sum(marks)
     average_marks = total_marks / len(marks)
